d8b72435e616a18ad71fc5101bdb64bf0782be43
Ahmed Bessrour <ahmed.bessrour@gmail.com>
2021-02-16 20:23:55 +0100

    Update variables name

--- typescript/src/main/typescript/movierental/customer.ts
+++ typescript/src/main/typescript/movierental/customer.ts
@@ -19,28 +19,28 @@ export class Customer {
     }
 
     public statement(): string {
-        let totalAmount: number = 0;
-        let frequentRenterPoints: number = 0;
+        let totalAmount = 0;
+        let frequentRenterPoints = 0;
         let result = "Rental Record for " + this.getName() + "\n";
 
-        for (const each of this.rentals) {
+        for (const movie of this.rentals) {
             let thisAmount = 0;
 
-            // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
+            // determine amounts for movie line
+            switch (movie.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    if (movie.getDaysRented() > 2) {
+                        thisAmount += (movie.getDaysRented() - 2) * 1.5;
                     }
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
+                    thisAmount += movie.getDaysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    if (movie.getDaysRented() > 3) {
+                        thisAmount += (movie.getDaysRented() - 3) * 1.5;
                     }
                     break;
             }
@@ -48,12 +48,12 @@ export class Customer {
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() === Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+            if ((movie.getMovie().getPriceCode() === Movie.NEW_RELEASE) && movie.getDaysRented() > 1) {
                 frequentRenterPoints++;
             }
 
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount.toFixed(1) + "\n";
+            result += "\t" + movie.getMovie().getTitle() + "\t" + thisAmount.toFixed(1) + "\n";
             totalAmount += thisAmount;
         }
 
